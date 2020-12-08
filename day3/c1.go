604ecb4733620eb2dd21d877202d2d43d621d4d4
avery <asommer999@gmail.com>
2020-12-08 11:51:08 -0500

    finished day3

---
+++ day3/c1.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+
+	list, _ := readLines("input3.txt")
+	var map2 [][]rune
+	var lineData []rune
+
+	for _, line := range list {
+		lineData = []rune{}
+		for _, elt := range line {
+			lineData = append(lineData, elt)
+		}
+		map2 = append(map2, lineData)
+	}
+
+	var xOF, yOF bool = false, false
+	var trees int = 0
+	var x, y int = 0, 0
+	var dx, dy int = 3, 1
+
+	for true {
+
+		x += dx
+		y += dy
+
+		if x >= len(map2[0]) {
+			xOF = true
+			x -= len(map2[0])
+		}
+		if y >= len(map2) {
+			yOF = true
+			y -= len(map2)
+		}
+
+		if xOF && yOF {
+			break
+		}
+
+		if map2[y][x] == 35 {
+			trees++
+		}
+
+	}
+	fmt.Println(trees)
+
+}
